9899c31b28055400e73b074e3deab4e1f7727b69
hardihsu <hardihsu@gmail.com>
2025-08-13 19:15:23 +0800

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import IOSChatInterface from './components/IOSChatInterface'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <IOSChatInterface />
+    </div>
+  )
+}
+
+export default App
